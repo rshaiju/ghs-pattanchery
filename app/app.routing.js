e5a3426f94c91c49d69325aaa6bbc9f6cf1a46af
Shaiju Rajan <s.rajan@devon.nl>
2019-05-05 10:02:01 +0530

    Used Html5mode

--- app/app.routing.js
+++ app/app.routing.js
@@ -2,8 +2,10 @@
 ///<reference path="../node_modules/@uirouter/angularjs/release/angular-ui-router.js">
 var app=angular.module('mySchoolApp');
 
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
     
+    $locationProvider.html5Mode(true);
+
     $urlRouterProvider.otherwise('/home');
 
     $stateProvider.state('home', {
